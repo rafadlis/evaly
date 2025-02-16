9282021bc920d8c5b0c331f91e7f75bfd692adc4
Fahrezi <fahreziadh@gmail.com>
2025-02-16 18:45:26 +0700

    Setup project with Tailwind CSS, UI components, and routing improvements

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
@@ -9,6 +9,7 @@ function HomeComponent() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>Hello</Button>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
+  plugins: [TanStackRouterVite({}), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
