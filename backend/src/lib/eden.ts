32558c813ede4cf998942920f06eac59a4abec4c
Fahrezi <fahreziadh@gmail.com>
2025-03-24 07:13:36 +0700

    Remove unused `eden.ts` file and clean up environment variable schema in `env.ts` by eliminating deprecated API URL keys for development and staging environments.

--- backend/src/lib/eden.ts
+++
@@ -1,9 +0,0 @@
-import { treaty } from "@elysiajs/eden";
-import { App } from "..";
-import { env } from "./env";
-
-export const $api = treaty<App>(env.API_URL, {
-  fetch: {
-    credentials: "include",
-  },
-});
