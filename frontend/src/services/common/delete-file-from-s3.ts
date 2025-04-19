8c0666dbced39e6141122de6f054c4d7894af8ff
Fahrezi <fahreziadh@gmail.com>
2025-04-19 21:14:23 +0700

    Comment out S3 delete logic in deleteFileFromS3 function for future reference
    - Temporarily commented out the S3 delete logic in the `deleteFileFromS3` function to simplify the implementation while retaining the structure for potential future use. This change aids in debugging and allows for easier modifications without removing the code entirely.

--- frontend/src/services/common/delete-file-from-s3.ts
+++ frontend/src/services/common/delete-file-from-s3.ts
@@ -1,13 +1,11 @@
-import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { env } from "../../lib/env";
-import { S3 } from "../../lib/s3";
 
 export async function deleteFileFromS3(key: string, isWithCdnUrl = true) {
   if (isWithCdnUrl) {
     key = key.replace(env.CLOUDFLARE_CDN_URL, "");
   }
-  await S3.send(new DeleteObjectCommand({
-    Bucket: env.CLOUDFLARE_BUCKET_NAME,
-    Key: key,
-  }));
+  // await S3.send(new DeleteObjectCommand({
+  //   Bucket: env.CLOUDFLARE_BUCKET_NAME,
+  //   Key: key,
+  // }));
 }
