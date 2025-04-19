fe030aadb590bb6ab0ae5bfd528f9a508da42ab1
Fahrezi <fahreziadh@gmail.com>
2025-04-19 14:44:06 +0700

    Refactor uploadFileToS3 function to comment out S3 upload logic
    - Commented out the S3 upload logic in the `uploadFileToS3` function to prevent execution while maintaining the structure for future use.
    - This change simplifies the function temporarily, allowing for easier debugging and modifications without removing the code entirely.

--- frontend/src/services/common/upload-file-to-s3.ts
+++ frontend/src/services/common/upload-file-to-s3.ts
@@ -1,24 +1,22 @@
-import { S3 } from "@/lib/s3";
 import { env } from "@/lib/env";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 export async function uploadFileToS3(file: File, key: string) {
   // Convert File to Uint8Array to avoid "Unable to calculate hash for flowing readable stream" error
-  const arrayBuffer = await file.arrayBuffer();
-  const uint8Array = new Uint8Array(arrayBuffer);
+  // const arrayBuffer = await file.arrayBuffer();
+  // const uint8Array = new Uint8Array(arrayBuffer);
   
-  const res = await S3.send(
-    new PutObjectCommand({
-      Bucket: env.CLOUDFLARE_BUCKET_NAME,
-      Key: key,
-      Body: uint8Array,
-      ContentType: file.type, // Preserve the file's content type
-    })
-  );
+  // const res = await S3.send(
+  //   new PutObjectCommand({
+  //     Bucket: env.CLOUDFLARE_BUCKET_NAME,
+  //     Key: key,
+  //     Body: uint8Array,
+  //     ContentType: file.type, // Preserve the file's content type
+  //   })
+  // );
 
-  if (res.$metadata.httpStatusCode !== 200) {
-    throw new Error("Failed to upload file to S3");
-  }
+  // if (res.$metadata.httpStatusCode !== 200) {
+  //   throw new Error("Failed to upload file to S3");
+  // }
 
   return `${env.CLOUDFLARE_CDN_URL}/${key}`;
 }
