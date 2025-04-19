4acf1b0548d46d64dc65a90b742d98ddace0d75a
Fahrezi <fahreziadh@gmail.com>
2025-04-20 03:15:37 +0700

    Enhance database connection configuration in index.ts
    - Added `neonConfig` settings to utilize WebSocket for improved database interactions.
    - Enabled `poolQueryViaFetch` for optimized query handling.
    - These changes enhance the performance and flexibility of the database connection setup.

--- frontend/src/lib/db/index.ts
+++ frontend/src/lib/db/index.ts
@@ -1,7 +1,11 @@
 import { env } from "../env";
 import * as schema from "./schema";
-import { neon } from "@neondatabase/serverless";
+import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true
 
 const db = drizzle(neon(env.DATABASE_URL), { schema });
 export default db;
