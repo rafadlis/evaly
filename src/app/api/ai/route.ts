e6d562b12c20be5bcec9b0c2e600f232d858bd19
fahrezi <fahrezi@goavatown.com>
2025-04-29 20:02:22 +0700

    Refactor environment variable handling in route.ts
    - Changed the method of accessing environment variables from `process.env` to `process`, simplifying the code structure.
    - Enhanced clarity in the GET endpoint response by directly returning the environment object.

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const env = process.env;
+  const env = process;
   // show all env variables
   return NextResponse.json(env);
 };
