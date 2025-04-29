b4cb95dc70684b31cae1bfeb8908d275509cce0d
fahrezi <fahrezi@goavatown.com>
2025-04-30 02:21:12 +0700

    Remove obsolete route.ts file to streamline API structure

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare"
-
-export const GET=async (req:Request)=>{
-    if (req.url.includes("fahrezi")){
-        return new Response(JSON.stringify(getCloudflareContext()))
-    }
-    return new Response("Hello World")
-}
