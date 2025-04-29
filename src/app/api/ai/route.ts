6daa4f36c28486056e938c913366fe29c26960dd
fahrezi <fahrezi@goavatown.com>
2025-04-29 20:07:39 +0700

    Enhance GET endpoint in route.ts to return structured request headers
    - Updated the GET method to convert request headers into a more accessible object format, improving the clarity of the response.
    - This change enhances the overall usability of the API by providing detailed context about incoming requests.

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -3,5 +3,8 @@ import { NextResponse } from "next/server";
 export const GET = async (request: Request) => {
   const env = process;
   // show all env variables
-  return NextResponse.json({env, headers: request.headers});
+  return NextResponse.json({
+    env,
+    headers: Object.fromEntries(request.headers.entries()),
+  });
 };
