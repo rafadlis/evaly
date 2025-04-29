fb80ab0f799cc3ac6c26428c2464579952d575a5
fahrezi <fahrezi@goavatown.com>
2025-04-29 20:03:08 +0700

    Enhance GET endpoint in route.ts to include request headers in the response
    - Updated the GET method to accept a Request parameter, allowing for the inclusion of request headers in the JSON response.
    - This change improves the clarity of the response by providing additional context about the incoming request.

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request: Request) => {
   const env = process;
   // show all env variables
-  return NextResponse.json(env);
+  return NextResponse.json({env, headers: request.headers});
 };
