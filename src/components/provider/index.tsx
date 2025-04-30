fc6e74a4c1efa5842c47bfe8701b40c6a5ebbfdc
Fahrezi <fahreziadh@gmail.com>
2025-04-30 13:36:20 +0700

    Add PostHog integration and update environment variables

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import TanstackQueryProvider from "./tanstack-query.provider";
 import NuqsProvider from "./nuqs.provider";
 import { ProgressBar } from "../shared/progress-bar";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import posthog from "posthog-js";
+import { env } from "@/lib/env.client";
+import SuspendedPostHogPageView from "./posthog-page-view";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageleave: true
+    });
+  }, []);
+
   return (
+    <PHProvider client={posthog}>
       <TanstackQueryProvider>
         <NuqsProvider>
           <ProgressBar className="bg-foreground fixed top-0 z-[100] h-1">
+            <SuspendedPostHogPageView />
             {children}
           </ProgressBar>
         </NuqsProvider>
       </TanstackQueryProvider>
+    </PHProvider>
   );
 };
 
--- src/lib/env.client.ts
+++ src/lib/env.client.ts
@@ -4,10 +4,14 @@ import { z } from "zod";
 // Define the environment schema
 const envSchema = z.object({
   NEXT_PUBLIC_URL: z.string(),
+  NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+  NEXT_PUBLIC_POSTHOG_HOST: z.string(),
 });
 
 // Parse the environment variables
 export const env = envSchema.parse({
   NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+  NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 });
   
