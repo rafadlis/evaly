9c64a071a51162f007fe4c44eeeb53273cd9838e
rafadlis <rahmatfadlis@gmail.com>
2025-05-03 18:46:53 +0700

    Refactor dashboard layout to integrate sidebar components

--- src/app/[locale]/(organizer)/dashboard/layout.tsx
+++ src/app/[locale]/(organizer)/dashboard/layout.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Provider from "./provider";
-import { DashboardNavbar } from "@/components/shared/dashboard-navbar";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarProvider,
+  SidebarInset,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "./_components/sidebar";
+import DynamicBreadcrumb from "@/components/shared/sidebar/dynamic-breadcrumb";
 
 export const dynamic = "force-static";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider>
-      <DashboardNavbar />
-      <main className="flex flex-col flex-1">{children}</main>
+      <SidebarProvider defaultOpen={false}>
+        <AppSidebar />
+        <SidebarInset className="w-full flex flex-col gap-3">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <DynamicBreadcrumb />
+            </div>
+          </header>
+          <div className="flex flex-1 flex-col gap-3 px-6 pb-6">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
     </Provider>
   );
 };
