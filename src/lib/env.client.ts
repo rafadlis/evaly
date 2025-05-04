187bbf89f3d38eeb843ddecbc9709590af1d036f
Fahrezi <fahreziadh@gmail.com>
2025-05-04 18:17:45 +0700

    Add Supabase dependencies and environment variables
    - Introduced new dependencies for Supabase, including `@supabase/ssr` and `@supabase/supabase-js`, in both `package.json` and `bun.lock`.
    - Updated environment schema in `env.client.ts` to include `NEXT_PUBLIC_SUPABASE_URL` and `NEXT_PUBLIC_SUPABASE_ANON_KEY` for better configuration management.

--- src/lib/env.client.ts
+++ src/lib/env.client.ts
@@ -6,6 +6,8 @@ const envSchema = z.object({
   NEXT_PUBLIC_URL: z.string(),
   NEXT_PUBLIC_POSTHOG_KEY: z.string(),
   NEXT_PUBLIC_POSTHOG_HOST: z.string(),
+  NEXT_PUBLIC_SUPABASE_URL: z.string(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
 });
 
 // Parse the environment variables
@@ -13,5 +15,6 @@ export const env = envSchema.parse({
   NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
   NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 });
-  
---
+++ src/lib/supabase.ts
@@ -0,0 +1,8 @@
+import { env } from "./env.client";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_KEY = env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabase;
