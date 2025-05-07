1e311f85b4d26028463a0055170b91dc1f20b97f
Fahrezi <fahreziadh@gmail.com>
2025-05-07 20:48:42 +0700

    Update warm-up configuration in sst.config.ts and import Link in page.tsx
    - Set the warm-up value to a fixed number in sst.config.ts for the MainWebsite component.
    - Replaced the import of Link from the progress-bar component with a direct import from next/link in page.tsx for improved clarity and functionality.

--- sst.config.ts
+++ sst.config.ts
@@ -12,7 +12,7 @@ export default $config({
   },
   async run() {
     new sst.aws.Nextjs("MainWebsite", {
-      warm: $app.stage === "main" ? 1 : undefined, // <= ?????
+      warm: 1,
       regions: ["us-east-1", "ap-southeast-1"],
       domain: {
         name: $app.stage === "main" ? "evaly.io" : "staging.evaly.io",
